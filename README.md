ecc181d46bbc7fd72251d76d4cb822955157243d
Sanu Hembrom <sanuhembrom3@gmail.com>
2025-04-27 06:36:25 +0000

    second-commit User Routes

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # Book_record_management
+
+Server >> Storing certain book data
+       >> User Register 
+       >> Subscriber
+
+
+This is book record management API Server/Backend for the library system or management of reords or manuals or books
+
+Fine system:
+User: 06/03/2023 - 06/06/2023
+07/06/2023 => 50*3=150/-
+
+3 months (Basic)
+6 months (Standard)
+12 months (Premium)
+
+If the subscription type is standard && if the subscription date is 06/03/2023
+=> then subscription valid till 06/09/2025
+
+>> 23/06/2023 => we need to pay a fine of 100+50
+
+
+missed by renewal date >> 50/-
+missed by subscription date >> 100/- 
+missed by renewal && subscription date >> 150/-
+
+
+
+
+
+# Routes and Endpoints
+
+## /users
+POST: Create a new user
+GET: Get all the user info here
+
+## /users/{id}
+GET: Get a user by their ID
+DELETE: Delete a user by id (chk if he/she still have an issued book) && (is there any fine to paid)
+
+## /users/subscription-details{id}
+GET: Get user subscription details 
+        >> Date of Subscription 
+        >> Valid till
+        >> Is there any fine
+
+## /books
+GET: Get all the books
+POST: Create/Add a new book
+
+## /books/{id}
+GET: Get all the book by id
+PUT: Update a book by id
+
+## /books/issued
+GET: Get all issued books
+
+## /books/issued/withFine
+GET: get all issued books with their fine
+
+## npm  init
+## npm i nodemon --save-dev
+## npm run dev
+
+
+...each
+"name": "Jane", 
+      "surname": "Doe",
+      "email": "user@gmail.com",
+      "subcriptionType": "Premium",
+      "subcriptionDate": "01/01/2022"
+
+...data
+  "data": {
+        "name": "sanu",
+        "surname": "Hembrom"
+  }
+
+name: "sanu",
+surname: "Hembrom",
+email: user@gmail.com
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const {users} = require("./data/users.json");
+const { users, admin } = require("./data/users.json");
+const { books } = require("./data/books.json");
+
 const app = express();
+app.use(express.json());
 
 const PORT = 8081;
 
@@ -9,6 +12,7 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.status(200).json({
         message: "Server is running",
+        data: "hey",
     });
 });
 
@@ -20,13 +24,97 @@ app.get("/",(req,res)=>{
 * Parameters: None
 */
 
-app.get("/users", (req, res) => {
+app.get("/users/:id", (req, res) => {
     res.status(200).json({
-        success: "true",
-        data: users
+        success: true,
+        data: users,
+    });
+});
+
+//http://localhost:8081/users/4
+/**
+ * Route: users
+ * Method: GET
+ * Description: Get single user by their id
+ * Access: Public
+ * Parameters: id
+ */
+app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(req.params);
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found"
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        message: "User found",
+        data: user,
+    });
+});
+
+/**
+ * Route: /users
+ * Method: POST
+ * Description: create a new user
+ * Access: Public
+ * Parameters: None
+ */
+app.post("/users", (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.status(201).json({ 
+        success: true,
+        message: "User created successfully",
+        data: newUser
+    });
+});
+
+/**
+ * Route: /users/:id
+ * Method: POST
+ * Description: Update a user by their id
+ * Access: Public
+ * Parameters: ID
+ */
+app.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+    
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
+    const updateUserData = users.map((each) => {
+        if (each.id === id) {
+            return { 
+                ...each,
+                ...data,
+
+            };
+        }
+        return each;
+    });
+    return res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+        data: updateUserData,
+    }); 
+});
+  
+
+app.get("/", (req, res) => {
+    res.status(404).json({
+        message: "This Route doesn't exist",
     });
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
